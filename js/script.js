0f2f3b3b1f9a5021faa0e3e2930304fd93353f07
adiletturdakunov2002@gmail.com <ilyas.yangurazov.iy@gmail.com>
2022-04-22 18:24:39 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-// let arr = [1, 2, 3, 4, 5]
+let arr = [1, 2, 3, 4, 5]
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i])
-// }
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
 
-// let map = arr.map((element) => {console.log(element)})
-// console.log(map)
-// let forEach = arr.forEach((element) => {console.log(element)})
-// console.log(forEach)
+let map = arr.map((element) => {console.log(element)})
+console.log(map)
+let forEach = arr.forEach((element) => {console.log(element)})
+console.log(forEach)
 
-// let arrObj = [
-//     { name: 'Aibek', surname: 'Aibekov', age: 23 },
-//     { name: 'Islam', surname: 'Islamov' }
-// ]
+let arrObj = [
+    { name: 'Aibek', surname: 'Aibekov', age: 23 },
+    { name: 'Islam', surname: 'Islamov' }
+]
 
-// for (let i = 0; i < arrObj.length; i++) {
-//     console.log(arrObj[i].name, arrObj[i].surname)
-// }
+for (let i = 0; i < arrObj.length; i++) {
+    console.log(arrObj[i].name, arrObj[i].surname)
+}
 
-// arrObj.map((element) => {
-//     console.log(element.name, element.surname)
-// })
+arrObj.map((element) => {
+    console.log(element.name, element.surname)
+})
 
-// arrObj.map(({ name, surname, age }) => {
-//     console.log(name, surname)
-// })
+arrObj.map(({ name, surname, age }) => {
+    console.log(name, surname)
+})
